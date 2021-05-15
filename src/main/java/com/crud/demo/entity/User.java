70c958101eb01ca4bad6a6398fb6f76f0f4b1970
sangameshpatil456 <sangmesh456@gmail.com>
2021-05-15 15:00:24 +0530

    Code Modified

--- src/main/java/com/crud/demo/entity/User.java
+++ src/main/java/com/crud/demo/entity/User.java
@@ -1,21 +1,10 @@
 package com.crud.demo.entity;
 
+import javax.persistence.*;
 import java.util.Date;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
-import lombok.Data;
 
 @Entity
 @Table(name = "user")
-@Data
 public class User {
 	
 	@Id
